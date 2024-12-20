d39dd1aa2c05eb8d4520fe7c83059532151cc390
S.P. Arrow <109922976+orizurux1000@users.noreply.github.com>
2024-12-20 14:44:33 -0500

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,59 @@
+# Contains a list of questions for the main file
+
+sets = [
+    {
+        "grade": 1,
+        "questions": [
+            {"question": "1 + 5", "answer": "2"},
+            {"question": "2 + 7", "answer": "9"},
+            {"question": "3 + 4", "answer": "7"},
+            {"question": "2 + 5", "answer": "7"},
+            {"question": "5 + 3", "answer": "8"},
+            {"question": "4 + 2", "answer": "6"},
+            {"question": "12 + 42", "answer": "54"},
+            {"question": "44 + 13", "answer": "57"},
+            {"question": "32 - 11", "answer": "21"},
+            {"question": "73 + 22", "answer": "95"},
+            {"question": "53 - 23", "answer": "30"},
+        ],
+    },
+    {
+        "grade": 2,
+        "questions": [
+
+        ],
+    },
+    {
+        "grade": 3,
+        "questions": [
+            {"question": "5 * 9", "answer": "45"},
+            {"question": "63 / 7", "answer": "9"},
+            {"question": "35 / 5", "answer": "7"},
+            {"question": "8 * 3", "answer": "24"},
+            {"question": "6 * 7", "answer": "42"},
+        ],
+    },
+    {
+        "level": 6,
+        "name": "find x",
+        "questions": [
+            {"question": "2x + 3 = 5", "answer": "1"},
+            {"question": "5x - 2 = 13", "answer": "3"},
+            {"question": "-4x + 3 = 15", "answer": "-3"},
+            {"question": "2x + 3 = 1", "answer": "-1"},
+            {"question": "5x - 4 = 21", "answer": "5"},
+        ]
+    },
+    {
+        "level": 7,
+        "name": "quadratic equations",
+        "note": "if more than one answer, input as \"a, b\" where a is less than b",
+        "questions": [
+            {"question": "x\u00B2 - 5x + 6 = 0", "answer": "2, 3"},
+            {"question": "x\u00B2 + 4x + 4 = 0", "answer": "-2"},
+            {"question": "x\u00B2 - 9x + 14 = 0", "answer": "2, 7"},
+            {"question": "x\u00B2 + 6x + 9 = 0", "answer": "-3"},
+            {"question": "x\u00B2 - 4x - 12 = 0", "answer": "-2, 6"},
+        ]
+    },
+]
